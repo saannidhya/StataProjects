434638e48d27c1ab8352204d6e6c167fe22eb0cc
Saani Rawat <rawatsa@uccob.uc.edu>
2025-01-24 18:24:28 -0500

    jmp_draft updates

--- ohio_taxation/code/road_spending_reg_discontinuity_by_mill_percent.R
+++ ohio_taxation/code/road_spending_reg_discontinuity_by_mill_percent.R
@@ -6,6 +6,7 @@
 # Log     : 
 #       06/16/2023: 
 #       12/16/2024: updated the code to have t-1 as the reference point
+#       1/24/2024 : updating based on CFR 2010
 #================================================================================================================#
 
 
@@ -27,13 +28,38 @@ source(paste0(code,"/housing_data_setup.R"))
 # computing the mean
 mean_mill <- purrr::map_dbl(dfs_agg_mill, ~ round(mean(as.numeric(.x$millagepercent), na.rm = TRUE),1))
 
+# Q. How much dollars are we talking per person on levies?
+# Each levy is approx 2 mills on average, mean house is 166k. So,
+2/1000 * 166000
+# $332 per household tax
+
+# separating based on above and below mean
 dfs_agg_mill_g_1.9 <- purrr::map2(dfs_agg_mill, mean_mill, ~ .x %>% filter(as.numeric(millagepercent)  > .y) %>% 
-                                    mutate(millagepercent = as.numeric(millagepercent) ) )
+                                    mutate(millagepercent = as.numeric(millagepercent),
+                                           treated = if_else(votes_pct_against > cutoff, 1, 0)  ) )
 
 dfs_agg_mill_l_1.9 <- purrr::map2(dfs_agg_mill, mean_mill, ~ .x %>% filter(as.numeric(millagepercent)  <= .y) %>% 
-                                    mutate(millagepercent = as.numeric(millagepercent) ) )
+                                    mutate(millagepercent = as.numeric(millagepercent),
+                                           treated = if_else(votes_pct_against > cutoff, 1, 0) ) )
 
-sort(unique(dfs_agg_mill$housing_roads_census_t_plus_4_matches$millagepercent))
+
+# adding covariates
+dfs_agg_gm_covs <- purrr::map(.x = dfs_agg_mill_g_1.9, ~ .x %>% 
+                             dplyr::left_join(y = census, by = c("tendigit_fips","vote_year")) %>%
+                             ungroup()
+)
+
+dfs_agg_lm_covs <- purrr::map(.x = dfs_agg_mill_l_1.9, ~ .x %>% 
+                                dplyr::left_join(y = census, by = c("tendigit_fips","vote_year")) %>%
+                                ungroup()
+)
+
+#=====================================================================================================#
+#=====================================================================================================#
+# running regressions: No Covariates ----
+#=====================================================================================================#
+#=====================================================================================================#
+
 
 #==================================================================================#
 # running regressions (aggregate) for millage size > 1.9 ----
@@ -64,6 +90,8 @@ png(paste0(plots,"/tes_l_1.9.png"))
 plot_te(tes_l_1.9, title = "Millage percent less than mean of 1.9%", subtitle = "Treatment Effect Estimates", caption = "")
 dev.off()
 
+
+map(dfs_agg_mill_l_1.9, tab)
 
 # append the two datasets regs_g_1.9 and regs_l_1.9
 tes_g_1.9 <- tes_g_1.9 %>% mutate(cat = "> mean tax levy",
@@ -101,41 +129,71 @@ ggplot(tes_size, aes(ord, robust_coef, color = cat)) +
   scale_color_manual(values = c("> mean tax levy" = "#1f77b4", "<= mean tax levy" = "#ff7f0e")) 
 
 
+#=====================================================================================================#
+#=====================================================================================================#
+# running regressions: With Covariates ----
+#=====================================================================================================#
+#=====================================================================================================#
+
+covs_list <- c("pop" ,"childpov" ,"poverty" ,"pctwithkids" ,"pctsinparhhld" ,"pctnokids" ,
+               "pctlesshs" ,"pcthsgrad" ,"pctsomecoll" ,"pctbachelors" ,"pctgraddeg" ,"unemprate" ,"medfamy" ,"pctrent" ,"pctown" ,"pctlt5" ,
+               "pct5to17" ,"pct18to64" ,"pct65pls" ,"pctwhite" ,"pctblack" ,"pctamerind" ,"pctapi" ,"pctotherrace" ,"pctmin" ,"raceherfindahl" ,
+               "pcthisp" ,"pctmarried" ,"pctnevermarr" ,"pctseparated" ,"pctdivorced" ,"lforcepartrate" ,"incherfindahl")
+
+covs_final_gm <- purrr::map(dfs_agg_gm_covs, ~find_covs(.x, y = "median_sale_amount", covs_list = covs_list))
+
+covs_final_lm <- purrr::map(dfs_agg_lm_covs, ~find_covs(.x, y = "median_sale_amount", covs_list = covs_list))
+
+#==================================================================================#
+# Above mean millage size
+#==================================================================================#
+
+gs_gm <- purrr::map2(covs_final_gm, dfs_agg_gm_covs, .f = function(x,y){
+                      rdrobust(  y = y$median_sale_amount,
+                                 x = y$votes_pct_against,
+                                 c = cutoff,
+                                 covs = y %>%
+                                   select(x) ,
+                                 all = TRUE, kernel = "tri", bwselect = "mserd", p = 1, q = 2)
+})
+tes_gs_gm <- te_tables(gs_gm)
+plot_te(tes_gs_gm, title = "Treatment Effect Estimates: Median House Price", subtitle = "With covariates")
+plot_te_recenter(tes_gs_gm, title = "Treatment Effect Estimates: Median House Price", subtitle = "With covariates")
+
+## with Time Fixed Effects ##
+
+# adding time dummies to data
+dfs_agg_covs_gm_w_tfe <- map(dfs_agg_gm_covs, ~ dummy_cols(.x, select_columns = c("year"), remove_first_dummy = TRUE) %>% relocate(starts_with("year_"), .after = "year"))
+
+# merging time dummies with covariates
+dfs_agg_covs_gm_tfe_names <- map(dfs_agg_covs_gm_w_tfe, ~ colnames(.x) %>% grep("year_", ., value = TRUE))
+covs_final_gm_w_tfe <- map2(covs_final_gm, dfs_agg_covs_gm_tfe_names, ~c(.x, .y))
 
 
+gs_gm <- purrr::map2(covs_final_gm_w_tfe, dfs_agg_covs_gm_w_tfe, .f = function(x,y){
+  rdrobust(  y = y$median_sale_amount,
+             x = y$votes_pct_against,
+             c = cutoff,
+             covs = y %>%
+               select(c('medfamy')) ,
+             all = TRUE, kernel = "tri", bwselect = "mserd", p = 1, q = 2)
+})
+tes_gs_gm <- te_tables(gs_gm)
+plot_te(tes_gs_gm, title = "Treatment Effect Estimates: Median House Price", subtitle = "With covariates")
+plot_te_recenter(tes_gs_gm, title = "Treatment Effect Estimates: Median House Price", subtitle = "With covariates")
 
 
+#==================================================================================#
+# Below mean millage size
+#==================================================================================#
 
+gs_lm <- purrr::map2(covs_final_lm, dfs_agg_lm_covs, .f = function(x,y){
+  rdrobust(  y = y$median_sale_amount,
+             x = y$votes_pct_against,
+             c = cutoff,
+             covs = y %>%
+               select(x) ,
+             all = TRUE, kernel = "tri", bwselect = "mserd", p = 1, q = 2)
+})
 
-##################################################
-
-for (i in seq(0,1.8, by = 0.1)){
-  dfs_agg_mill_g_1.9 <- purrr::map(dfs_agg_mill, ~ .x %>% filter(millage_percent > (1.9 + i) ))
-  
-  dfs_agg_mill_l_1.9 <- purrr::map(dfs_agg_mill, ~ .x %>% filter(millage_percent <= (1.9 - i) ))
-
-  #==================================================================================#
-  # running regressions (aggregate) for millage size > 1.9 ----
-  #==================================================================================#
-  
-  ### |- median sale amount ####
-  
-  # storing univariate RDD models (outcome vs running variable, no covariates) from t-2 to t+10
-  regs_g_1.9 <- purrr::map(.x = dfs_agg_mill_g_1.9, ~ rdrobust::rdrobust(y = .x$median_sale_amount, x = .x$votes_pct_for, c = cutoff, all = TRUE))
-
-  #==================================================================================#
-  # running regressions (aggregate) for millage size <= 1.9 ----
-  #==================================================================================#
-  regs_l_1.9 <- purrr::map(.x = dfs_agg_mill_l_1.9, ~ rdrobust::rdrobust(y = .x$median_sale_amount, x = .x$votes_pct_for, c = cutoff, all = TRUE))
-  
-  
-  
-  df_compare <- data.frame(regs_g_1.9 = purrr::map_dbl(regs_g_1.9, ~ .x$coef[2]), 
-                           regs_l_1.9 = purrr::map_dbl(regs_l_1.9, ~ .x$coef[2]),
-                           diff = purrr::map_dbl(regs_g_1.9, ~ .x$coef[2]) - purrr::map_dbl(regs_l_1.9, ~ .x$coef[2]))
-  
-  print(paste0("cutoffs are: ", 1.9 + i, " and ", 1.9 - i))
-  print(df_compare)
-
-}
 
